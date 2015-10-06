7049992c3697bf8de18d6f74e2df5842e4799a78
Frianto <rf.moerdowo@gmail.com>
2015-10-06 20:06:02 +0700

    ch

--- javascripts/custom-repos.js
+++ javascripts/custom-repos.js
@@ -3,46 +3,6 @@
 var optInRepos = [
   'dashing',
   'active_merchant',
-  'liquid',
-  'superdb',
-  'active_shipping',
-  'shopify_app',
-  'identity_cache',
-  'skeleton-theme',
-  'statsd-instrument',
-  'shopify_theme',
-  'shopify_api',
-  'sarama',
-  'Timber',
-  'shopify-css-import',
-  'active_fulfillment',
-  'shopify_python_api',
-  'wolverine',
-  'verdict',
-  'shopify_django_app',
-  'browser_sniffer',
-  'active_utils',
-  'embedded-app-example',
-  'connect-googleapps',
-  'omniauth-shopify-oauth2',
-  'shopify-fulfillment-integration',
-  'sysv_mq',
-  'money',
-  'sync_app_demo',
-  'rails_parallel',
-  'dukpt',
-  'splunk-auth-proxy',
-  'magnet',
-  'goreferrer',
-  'asset_cloud',
-  'minesweeper',
-  'toxiproxy',
-  'toxiproxy-ruby',
-  'grizzly_ber',
-  'semian',
-  'go-lua',
-  'goluago',
-  'shipit-engine'
 ]
 
 
@@ -73,16 +33,6 @@ var customRepos = [
 
 // Custom repo language, different than that defined by GitHub
 var customRepoLanguage = {
-  /*'liquid': 'Liquid',
-  'Timber': 'Liquid',
-  'skeleton-theme': 'Liquid',
-  'dashing': 'Ruby',
-  'shopify_theme': 'Ruby',
-  'Shopify-Developer-Book': 'Ruby',
-  'offsite-gateway-sim': 'Ruby',
-  'shopify.github.com': 'JavaScript',
-  'batman': 'JavaScript'
-  */
   'playbook':'template',
   'babyclothes':'template',
   'distroshop':'template',
@@ -104,10 +54,6 @@ var customRepoLanguage = {
 }
 
 // Custom repo avatars. Dimensions should be 40x40
-// - Be sure a custom repo doesn't have the same name as a Shopify one, or a one will be overridden
 var customRepoAvatar = {
-  'Timber': '/images/repo-avatars/timber.gif',
-  'dashing': '/images/repo-avatars/dashing.gif',
-  'superdb': '/images/repo-avatars/super-debugger.gif',
-  'batman': '/images/repo-avatars/batmanjs.gif'
+  //'batman': '/images/repo-avatars/batmanjs.gif'
 }
